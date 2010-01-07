7c4ee7328ccc0a41433224b5bed54c295f25523c
Marc-André <marcandre.lureau@gmail.com>
2010-01-07 13:16:16 +0000

    hls-player: add copyright/license

--- hls-player.py
+++ hls-player.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python
+# -*- Mode: Python -*-
+# vi:si:et:sw=4:sts=4:ts=4
+#
+# Copyright (C) 2009-2010 Fluendo, S.L. (www.fluendo.com).
+# Copyright (C) 2009-2010 Marc-Andre Lureau <marcandre.lureau@gmail.com>
+# Copyright (C) 2010 Zaheer Abbas Merali  <zaheerabbas at merali dot org>
+# Copyright (C) 2010 Andoni Morales Alastruey <ylatuya@gmail.com>
+
+# This file may be distributed and/or modified under the terms of
+# the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+# This file is distributed without any warranty; without even the implied
+# warranty of merchantability or fitness for a particular purpose.
+# See "LICENSE.GPL" in the source distribution for more information.
 
 import sys
 import urlparse
